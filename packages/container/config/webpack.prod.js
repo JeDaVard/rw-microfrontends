f7af6ababfeeb910d357a19d8095a9208ece11c3
Davit Vardanyan <jedavard@gmail.com>
2020-12-13 12:16:31 +0400

    done with aws cloudfront autoinvalidation

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -9,7 +9,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
-    publicPath: '/container/latest/123/'
+    publicPath: '/container/latest/'
   },
   plugins: [
     new ModuleFederationPlugin({
