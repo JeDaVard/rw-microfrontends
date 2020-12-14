e0adbcdb75b24238680d5ec8b0487e309e943399
Davit Vardanyan <jedavard@gmail.com>
2020-12-14 22:34:41 +0400

    working on routing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Switch, Route, Router } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
@@ -9,16 +9,16 @@ const makeUniqueClasses = createGenerateClassName({
   productionPrefix: 'marketing'
 })
 
-export default () => {
+export default (props) => {
   return (
     <div>
       <StylesProvider generateClassName={makeUniqueClasses}>
-        <BrowserRouter>
+        <Router history={props.history}>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
             <Route path="/" component={Landing} />
           </Switch>
-        </BrowserRouter>
+        </Router>
       </StylesProvider>
     </div>
   );
