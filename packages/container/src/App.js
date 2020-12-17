6db8e1fda9425eff45d47f0a84cfbd86478ce239
Davit Vardanyan <jedavard@gmail.com>
2020-12-17 19:16:34 +0400

    progress loading

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import {createGenerateClassName, StylesProvider} from "@material-ui/core/styles";
 import Header from "./components/Header";
+import Progress from "./components/Progress";
 
 const AuthAppLazy = React.lazy(() => import('./components/AuthApp'))
 const MarketingAppLazy = React.lazy(() => import('./components/MarketingApp'))
@@ -16,7 +17,7 @@ export default () => {
           <BrowserRouter>
               <div>
                   <Header />
-                  <React.Suspense fallback={<h1>Loading...</h1>}>
+                  <React.Suspense fallback={<Progress />}>
                       <Switch>
                           <Route path="/auth" component={AuthAppLazy}/>
                           <Route path="/" component={MarketingAppLazy}/>
