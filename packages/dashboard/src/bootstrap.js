3fecd54e023e62f52a10abc06561a82ac5ccfaf4
Davit Vardanyan <jedavard@gmail.com>
2020-12-24 13:19:47 +0400

    new vue app

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history'
+
+// Mount function to start up the app
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
+  // defaultHistory is used for isolated mode (dev mode)
+  const history = defaultHistory || createMemoryHistory({
+    // for "double click to navigate" issue
+    initialEntries: [initialPath]
+  });
+  if (!!onNavigate) history.listen(onNavigate);
+
+  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
+
+  return {
+    onParentNavigate: (location) => {
+      const nextPathname = location.pathname
+      const pathname = history.location.pathname
+
+      if (pathname !== nextPathname) {
+        history.push(nextPathname)
+      }
+    }
+  }
+};
+
+// If we are in development and in isolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_auth-dev-root');
+
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// We are running through container
+// and we should export the mount function
+export { mount };
